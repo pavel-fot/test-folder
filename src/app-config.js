744a21de53d7b7f20f32e5015b2ccdcfc79eeb17
Pavel Fot <pavel.fot@gmail.com>
2020-01-27 14:36:08 +0200

    invite commit 5

--- src/app-config.js
+++ src/app-config.js
@@ -5,6 +5,4 @@ export const APP_PREFIX = 'SN-FRONT::INVITE-APP';
 export const appDomain = domainSelector(_ => _.inviteApp);
 export const reset = actionCreator(`${APP_PREFIX}::RESET-APP`);
 
-export const FOLDER_BACK_ID = 'FOLDER_BACK_ID';
-
-export const PAGE_ACTION_CONVERT = 'convert';
+sdfdggf
